8d1467ed65f0a87dd443a2fdb7c2e0f59808ceb7
prrrcl <adrianporcelnavarro@hotmail.com>
2019-08-15 12:33:54 +0200

    fixed mailer

--- helpers/nodemailer.js
+++ helpers/nodemailer.js
@@ -3,23 +3,22 @@ require('dotenv').config();
 
 function sendContactMail (mail, token) {
   const transporter = nodemailer.createTransport({
-    service: "hotmail",
+    service: "gmail",
     auth: {
-      user: 'ementhboxmanager',
+      user: 'ementhboxmanager@gmail.com',
       pass: 'prrrclementh'
     }
   });
 
   const mailOptions = {
-    from: 'hi@prrrcl.com',
+    from: 'ementhboxmanager@gmail.com',
     to: mail,
     subject: `Hey! Let's start train!`,
     text: '',
     html: `
       <p>Hi!</p>
       <p>You are invited to our Box! Click there for get full acces:</p>
-      <p><a href="${process.env.MAILURL}/${token}/${mail}"></a></p>
-      <p>You are invited to our Box! Click there for get full acces:</p>`
+      <p><a href="http://localhost:3000/completesignup/${token}/${mail}">AQUí</a></p>`
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
